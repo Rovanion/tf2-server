a7aabe4eeb94c6c6d1dab636a33af527a8cab0e1
Rovanion Luckey <rovanion.luckey@gmail.com>
2016-12-03 00:21:47 +0100

    Initial commit.

---
+++ tf2.bash
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+screenID="tf2-console"
+
+cd $(dirname $0)
+
+function startTF2 {
+    ./update-tf2.bash
+    screen -S $screenID -d -m tf2/srcds_run -consolelog /var/log/srcds/tf2.log -game tf +sv_pure 1 +map ctf_2fort +maxplayers 32 +ip 130.236.245.45 &
+}
+
+function watchForRestart {
+tail --retry -F -n 0 /var/log/srcds/tf2.log |\
+    while read line; do
+	if echo "$line" | grep -E -q 'Your server needs to be restarted in order to receive the latest update.'; then
+	    echo Trying to restart server!
+	    screen -S $screenID -X quit
+	    startTF2
+	    screen -r $screenID
+	fi
+    done
+}
+
+
+startTF2
+# bash ~/source/chrooms/chrooms/log-watcher.bash &
+touch /var/log/srcds/tf2.log
+watchForRestart &
+screen -r $screenID
+echo Exiting tf2.bash
